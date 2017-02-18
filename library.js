d196b4624555af21990007de663d8bab4c885c9e
Jorgeewa <georgeewauche@yahoo.com>
2017-02-18 09:53:53 +0100

    nodebb
    nodebb

--- library.js
+++ library.js
@@ -31,7 +31,7 @@
 
 	var constants = Object.freeze({
 			type: 'oauth2',	// Either 'oauth' or 'oauth2'
-			name: 'powerful-shelf-45643.herokuapp',	// Something unique to your OAuth provider in lowercase, like "github", or "nodebb"
+			name: 'nodebb',	// Something unique to your OAuth provider in lowercase, like "github", or "nodebb"
 			oauth: {
 				requestTokenURL: '',
 				accessTokenURL: '',
